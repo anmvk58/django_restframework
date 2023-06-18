f4bcd8aa32204acaedd9e3d9ef39f992bdb88986
unknown <an.cnttk58@gmail.com>
2023-06-18 15:43:34 +0700

    fix

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
@@ -30,7 +30,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-class UserProfile(AbstractUser, PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
